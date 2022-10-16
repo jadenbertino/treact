ca588ab1905285724d27acc71b4d492b25e6ce90
Jaden <jaden@bertinofamily.com>
2022-10-16 14:07:10 -0700

    create faq & basic toggle

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-function openMenu() {
-  document.body.classList += " menu--open"
-  /* 
-  - goes to the object "document -> body -> list of it's classes" then appends the given class
-  - You need the space so that it is separated from any existing classes
-  when it gets appended 
-  */ 
-}
-
-function closeMenu() {
-  document.body.classList.remove('menu--open')
-  /* 
-  - goes to the object "document -> body -> list of it's classes" then removes the given class
-  */
-}
-
 // ——————
 
-function setActive(question) {
-  let questions = ["q1__active", "q2__active", "q3__active", "q4__active", "q5__active", "q6__active"];
-  let index = questions.indexOf(question);
-  if (index > -1) {
-    questions.splice(index, 1);
+function setActive(question_num) {
+  let question = document.getElementById(question_num);
+
+  if (question.classList.contains('active')) {
+    // if question is active, then set as inactive
+    question.classList.remove('active');
   }
-  document.body.classList.remove(questions); // unfocus any other questions
-  document.body.classList += ` ${question}` // focus question clicked
+
+  else {
+    // if question is inactive, then set as active
+    question.classList.add('active');
+/*
+    // also set all other questions as inactive
+    let all__questions = document.getElementsByClassName('question')
+    for (let i = 0; i < questions.length; i++) {
+      if (questions[i].classList.contains('active')) {
+        questions[i].classList.remove('active')
+      }
+    }
+
+    
+  else {
+    // if question is not currently focused, then
+    // 1) unfocus all other questions
+    let questions = ["q1", "q2", "q3", "q4", "q5", "q6"];
+    let index = questions.indexOf(question);
+    if (index > -1) {
+      questions.splice(index, 1);
+    }
+    document.body.classList.remove(questions);
+
+    // 2) focus current question 
+    answer__toggle += question;
+  }
+
+    */
+  }
+
 }
+
