75d7303215be185ed818d3fa9e23d2aa0a72adda
Jaden <jaden@bertinofamily.com>
2022-10-18 10:11:55 -0700

    Create solo focus effect on FAQ

--- index.js
+++ index.js
@@ -11,32 +11,35 @@ function setActive(question_num) {
   else {
     // if question is inactive, then set as active
     question.classList.add('active');
-/*
+
     // also set all other questions as inactive
-    let all__questions = document.getElementsByClassName('question')
+    let questions = document.getElementsByClassName('question')
     for (let i = 0; i < questions.length; i++) {
-      if (questions[i].classList.contains('active')) {
-        questions[i].classList.remove('active')
+      let q = questions[i];
+      if (q.classList.contains('active') && q.id != question_num) {
+        q.classList.remove('active');
       }
     }
+  }
+}
 
     
-  else {
-    // if question is not currently focused, then
-    // 1) unfocus all other questions
-    let questions = ["q1", "q2", "q3", "q4", "q5", "q6"];
-    let index = questions.indexOf(question);
-    if (index > -1) {
-      questions.splice(index, 1);
-    }
-    document.body.classList.remove(questions);
+  // else {
+  //   // if question is not currently focused, then
+  //   // 1) unfocus all other questions
+  //   let questions = ["q1", "q2", "q3", "q4", "q5", "q6"];
+  //   let index = questions.indexOf(question);
+  //   if (index > -1) {
+  //     questions.splice(index, 1);
+  //   }
+  //   document.body.classList.remove(questions);
 
-    // 2) focus current question 
-    answer__toggle += question;
-  }
+  //   // 2) focus current question 
+  //   answer__toggle += question;
+  // }
 
-    */
-  }
-
-}
+  //   */
+  // }
+// 
+// }
 
